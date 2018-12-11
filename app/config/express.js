e30ec05dcd54fd21db86e20c9c510cf52f17b711
lu-chang <285605224@qq.com>
2018-12-11 20:29:13 +0800

    update voting module

--- app/config/express.js
+++ app/config/express.js
@@ -3,10 +3,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import config from './config';
 import initData from '../utils/initData';
+import pollIndexSchedule from '../schedule/pollIndexSchedule';
 import supernodeSchedule from '../schedule/supernodeSchedule';
 import nodeScheduleSchedule from '../schedule/nodeSchedule';
 import coinmarketcapSchedule from '../schedule/coinmarketcapSchedule';
 import clientWS from '../websocket/clientWS';
+import { NEMLibrary, NetworkTypes } from "nem-library";
 
 module.exports = () => {
 	console.log('init express...');
@@ -50,6 +52,11 @@ module.exports = () => {
 	});
 
 	let server = http.createServer(app);
+	// init NEMLibrary 
+	if(config.network==68)
+		NEMLibrary.bootstrap(NetworkTypes.MAIN_NET);
+	else
+		NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
 	// init data
 	initData.init();
 	// schedule fetch data
@@ -58,12 +65,18 @@ module.exports = () => {
 	nodeScheduleSchedule.scheduleFetchNode();
 	// schedule fetch price from coinmarketcap
 	coinmarketcapSchedule.scheduleFetchPrice();
+	// schedule fetch poll index
+	pollIndexSchedule.schedulePollIndex();
 
 	// websocket
 	clientWS.initUnconfirmedTransactionWS(server);
 	clientWS.initTransactionWS(server);
 	clientWS.initBlockWS(server);
 	clientWS.initMosaicWS(server);
+	
+	process.on('uncaughtException', function(e) {
+	　　console.log(e);
+	});
 	
 	server.listen(config.port, function(){
 		console.log('app started, listening on port:', config.port);
